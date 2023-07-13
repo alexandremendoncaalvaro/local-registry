704fdc957935b1bc6da99e99e7b822018e0e33dc
alexandremendoncaalvaro <alexandre.alvaro@hotmail.com>
2023-07-13 20:33:24 -0300

    atualizando exemplo

--- test-server.js
+++ test-server.js
@@ -2,8 +2,32 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta http-equiv="refresh" content="10">
+      <style>
+        html, body {
+          height: 100%;
+          margin: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color: #f0f0f0;
+          font-family: Arial, sans-serif;
+        }
+        h1 {
+          font-size: 2.5em;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Hello World!</h1>
+    </body>
+    </html>
+  `);
 });
 
 server.listen(8080, '0.0.0.0', () => {
